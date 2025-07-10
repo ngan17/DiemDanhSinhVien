5182ea6c0de803e4f36713210f31c1c0f2b14654
Your Name <youremail@example.com>
2025-07-10 23:52:33 +0700

    Fist

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,104 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(home: MyHomePage());
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                'https://tse3.mm.bing.net/th/id/OIP.7Cb5PBLikpovOIgwkF2YkgHaHW?rs=1&pid=ImgDetMain&o=7&rm=30',
+              ),
+              radius: 20,
+            ),
+            SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Võ Trọng Nhất', style: TextStyle(color: Colors.white)),
+                Text(
+                  'MSSV: 200123265',
+                  style: TextStyle(color: Colors.white, fontSize: 12),
+                ),
+              ],
+            ),
+          ],
         ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Thông tin sinh viên'),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+              ListTile(
+                leading: Icon(Icons.lock),
+                title: Text('Đổi mật khẩu'),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Điều chỉnh các chức năng'),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+              ListTile(
+                leading: Icon(Icons.feedback),
+                title: Text('Góp ý ứng dụng'),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+              ListTile(
+                leading: Icon(Icons.notifications),
+                title: Text('Thông báo'),
+                trailing: Switch(value: true, onChanged: (value) {}),
+              ),
+              ListTile(
+                leading: Icon(Icons.logout, color: Colors.red),
+                title: Text('Đăng xuất', style: TextStyle(color: Colors.red)),
+                trailing: Icon(Icons.arrow_forward_ios, color: Colors.red),
+              ),
+              SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Phiên bản 1.4.8',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Trang chủ'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Lịch học',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Điểm danh'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Cá nhân'),
+        ],
       ),
     );
   }
